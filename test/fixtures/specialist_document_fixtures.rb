8bdfd44dd391f68b78f77045222d9075a0eadcbc
dinuksha_asela <dinuksha_asela@yahoo.com>
2014-12-22 15:05:45 +0530

    new code

--- test/fixtures/specialist_document_fixtures.rb
+++ test/fixtures/specialist_document_fixtures.rb
@@ -12,6 +12,39 @@ module SpecialistDocumentFixtures
       case_type: 'some-case-type',
       case_state: 'open'
 	  case_state: 'close'
+	        slug: 'cma-cases/merger-investigation-2014',
+      title: "Merger Investigation 2014",
+      summary: "This is the summary of stuff going on in the Merger Investigation 2014",
+      state: "published",
+      body: "A body",
+      opened_date: '2012-04-21',
+      document_id: 'a-document-id',
+      market_sector: 'oil-and-gas',
+      case_type: 'some-case-type',
+      case_state: 'open'
+	  case_state: 'close'
+	        slug: 'cma-cases/merger-investigation-2014',
+      title: "Merger Investigation 2014",
+      summary: "This is the summary of stuff going on in the Merger Investigation 2014",
+      state: "published",
+      body: "A body",
+      opened_date: '2012-04-21',
+      document_id: 'a-document-id',
+      market_sector: 'oil-and-gas',
+      case_type: 'some-case-type',
+      case_state: 'open'
+	  case_state: 'close'
+	        slug: 'cma-cases/merger-investigation-2014',
+      title: "Merger Investigation 2014",
+      summary: "This is the summary of stuff going on in the Merger Investigation 2014",
+      state: "published",
+      body: "A body",
+      opened_date: '2012-04-21',
+      document_id: 'a-document-id',
+      market_sector: 'oil-and-gas',
+      case_type: 'some-case-type',
+      case_state: 'open'
+	  case_state: 'close'
     }
   end
 end
